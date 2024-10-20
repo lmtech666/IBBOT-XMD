a837bcc701974d344398ce1499996f5a93d611eb
TCRONEB <01worldoftechnology@gmail.com>
2024-10-20 16:21:06 +0200

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -19,7 +19,7 @@ const config = {
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
   MODE: process.env.MODE || "public",
   OWNER_NAME: process.env.OWNER_NAME || "TCRONEB-HACKX",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "+263716394979 ",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "+263716394979",
   GEMINI_KEY: process.env.GEMINI_KEY || "AIzaSyCUPaxfIdZawsKZKqCqJcC-GWiQPCXKTDc",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
 };
